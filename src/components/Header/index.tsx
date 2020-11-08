138415b1178d5b64f9067fe9b8de3228970f9c79
tadashi <tadashi.b@gmail.com>
2020-11-08 11:45:58 -0300

    App

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,12 +15,8 @@ const Header: React.FC<IHeaderProps> = ({ openModal }) => (
       <img src={Logo} alt="GoRestaurant" />
       <nav>
         <div>
-          <button
-            type="button"
-            onClick={() => {
-              /* TODO OPEN MODAL */
-            }}
-          >
+          {/** Passamos a função 'openModal' dentro de 'onClick' */}
+          <button type="button" onClick={openModal}>
             <div className="text">Novo Prato</div>
             <div className="icon">
               <FiPlusSquare size={24} />
